b62f6663883e1c0aae420268398347b0f9a86633
deutschkihun@gmail.com <deutschkihun@gmail.com>
2023-01-11 17:31:43 +0900

    Update footer

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Blog by @deutschkihun',
+  author: 'Kihun Kim',
+  headerTitle: 'blog-deutschkihun',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
@@ -10,12 +10,9 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'deutschkihun@gmail.com',
+  github: 'https://github.com/deutschkihun',
+  linkedin: 'https://www.linkedin.com/in/kihun-kim-b35b73174/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
